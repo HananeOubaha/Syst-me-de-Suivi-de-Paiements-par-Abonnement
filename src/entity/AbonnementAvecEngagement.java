5b5a70f41236087416c94601a6f4ef546d0f295f
Hanane Oubaha <Hananeouba03@gmail.com>
2025-09-23 16:34:21 +0100

    feat : j'ai cree les sous classes d'abonnement   : https://hananeoubaha.atlassian.net/browse/SDSPPA-4

--- src/entity/AbonnementAvecEngagement.java
+++ src/entity/AbonnementAvecEngagement.java
@@ -1,5 +1,21 @@
 package entity;
 
-public class AbonnementAvecEngagement extends Abonnement{
+import java.time.LocalDate;
 
+public class AbonnementAvecEngagement extends Abonnement {
+    private int dureeEngagementMois;
+
+    // CONstructeur
+    public AbonnementAvecEngagement(String nomService, double montantMensuel, LocalDate dateDebut, LocalDate dateFin, StatutAbonnement statut, int dureeEngagementMois) {
+        super(nomService, montantMensuel, dateDebut, dateFin, statut);
+        this.dureeEngagementMois = dureeEngagementMois;
+    }
+
+    // Getters / setters
+    public int getDureeEngagementMois() {
+        return dureeEngagementMois;
+    }
+    public void setDureeEngagementMois(int dureeEngagementMois) {
+        this.dureeEngagementMois = dureeEngagementMois;
+    }
 }
--- src/entity/AbonnementSansEngagement.java
+++ src/entity/AbonnementSansEngagement.java
@@ -1,5 +1,10 @@
 package entity;
 
-public class AbonnementSansEngagement {
+import java.time.LocalDate;
 
+public class AbonnementSansEngagement extends Abonnement {
+    // CONStructeur
+    public AbonnementSansEngagement(String nomService, double montantMensuel, LocalDate dateDebut, LocalDate dateFin, StatutAbonnement statut) {
+        super(nomService, montantMensuel, dateDebut, dateFin, statut);
+    }
 }
