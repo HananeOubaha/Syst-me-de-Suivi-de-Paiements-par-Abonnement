838651bc708b86b0d54ae4cde1766ce14a3f8937
Hanane Oubaha <Hananeouba03@gmail.com>
2025-09-23 16:38:52 +0100

    feat : j'ai cree la classe paiement   : https://hananeoubaha.atlassian.net/browse/SDSPPA-5

--- src/entity/Paiement.java
+++ src/entity/Paiement.java
@@ -1,4 +1,61 @@
 package entity;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class Paiement {
+    private String idPaiement;
+    private String idAbonnement;
+    private LocalDate dateEcheance;
+    private LocalDate datePaiement;
+    private String typePaiement;
+    private StatutPaiement statut;
+
+    public Paiement(String idAbonnement, LocalDate dateEcheance, LocalDate datePaiement, String typePaiement, StatutPaiement statut) {
+        this.idPaiement = UUID.randomUUID().toString(); // auto
+        this.idAbonnement = idAbonnement;
+        this.dateEcheance = dateEcheance;
+        this.datePaiement = datePaiement;
+        this.typePaiement = typePaiement;
+        this.statut = statut;
+    }
+
+    // Getters & setters
+    public String getIdPaiement() {
+        return idPaiement;
+    }
+    public String getIdAbonnement() {
+        return idAbonnement;
+    }
+    public void setIdAbonnement(String idAbonnement) {
+        this.idAbonnement = idAbonnement;
+    }
+
+    public LocalDate getDateEcheance() {
+        return dateEcheance;
+    }
+    public void setDateEcheance(LocalDate dateEcheance) {
+        this.dateEcheance = dateEcheance;
+    }
+
+    public LocalDate getDatePaiement() {
+        return datePaiement;
+    }
+    public void setDatePaiement(LocalDate datePaiement) {
+        this.datePaiement = datePaiement;
+    }
+
+    public String getTypePaiement() {
+        return typePaiement;
+    }
+    public void setTypePaiement(String typePaiement) {
+        this.typePaiement = typePaiement;
+    }
+
+    public StatutPaiement getStatut() {
+        return statut;
+    }
+    public void setStatut(StatutPaiement statut) {
+        this.statut = statut;
+    }
 }
