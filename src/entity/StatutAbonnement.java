c8bdaf6d125f672d65a649043cba39c6b6ebf315
Hanane Oubaha <Hananeouba03@gmail.com>
2025-09-27 22:38:15 +0100

    fix : optimisation et fixation des problems https://hananeoubaha.atlassian.net/browse/SDSPPA-9

--- src/entity/StatutAbonnement.java
+++
@@ -1,7 +0,0 @@
-package entity;
-
-public enum StatutAbonnement {
-    Active,
-    Suspendu,
-    Resile
-}
--- src/entity/StatutPaiement.java
+++
@@ -1,7 +0,0 @@
-package entity;
-
-public enum StatutPaiement {
-    Paye,
-    Non_Pay,
-    En_Retard
-}
